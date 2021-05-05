708fa4738e9c36081b1b784adf2160af89b62bed
Zgurskiy11 <zgurskiy11@gmail.com>
2021-05-05 17:48:51 -0600

    1st

---
+++ src/test/java/com/cybertek/tests/day01_selenium_intro/SeleniumTest.java
@@ -0,0 +1,25 @@
+package com.cybertek.tests.day01_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        // 1) Set up Web driver:
+
+        WebDriverManager.chromedriver().setup();
+
+        //2) Create the instance of the Chrome driver
+        WebDriver driver=new ChromeDriver();
+
+
+        // 3) Test if driver is working
+         driver.get("https://www.google.com");
+
+
+
+
+
+    }
+}
